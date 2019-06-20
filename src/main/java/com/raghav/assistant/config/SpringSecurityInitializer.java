32cab201023dfea942717167c32191749dd7af8d
raghav19897 <52039552+raghav19897@users.noreply.github.com>
2019-06-20 21:15:16 +0530

    Add files via upload

---
+++ src/main/java/com/raghav/assistant/config/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package com.raghav.assistant.config;
+
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+    //no code needed
+}
